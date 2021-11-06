2694b663a3fc898e512c1316dba39f39fc4eefe6
Tommi Paananen <tpaananen@users.noreply.github.com>
2021-11-06 08:46:51 -0700

    Update EditGoToRecentFile.cs

--- EditGoToRecentFile.cs
+++ EditGoToRecentFile.cs
@@ -5,24 +5,17 @@ public class EditGoToRecentFile : VisualCommanderExt.ICommand
 {
     public void Run(EnvDTE80.DTE2 DTE, Microsoft.VisualStudio.Shell.Package package) 
     {
-        serviceProvider = package as System.IServiceProvider;
-        SetSearchCurrentDocument(false);
+        System.IServiceProvider serviceProvider = package as System.IServiceProvider;
+        SetSearchCurrentDocument(serviceProvider, false);
         DTE.ExecuteCommand("Edit.GoToRecentFile");
     }
 
-    private void SetSearchCurrentDocument(bool value)
+    private void SetSearchCurrentDocument(System.IServiceProvider serviceProvider, bool value)
     {
-        System.Type t = serviceProvider.GetService(typeof(SVsNavigateToService)).
-            GetType().Assembly.GetType("Microsoft.VisualStudio.PlatformUI.NavigateToSettings");
-        object o = t.GetProperty("Instance",
-            System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static).
-            GetValue(null);
-        t.GetProperty("SearchCurrentDocument",
-            System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance).
-            SetValue(o, value);
+        System.Type type = serviceProvider.GetService(typeof(SVsNavigateToService)).GetType().Assembly.GetType("Microsoft.VisualStudio.PlatformUI.NavigateToSettings");
+        object obj = type.GetProperty("Instance", System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static).GetValue(null);
+        type.GetProperty("SearchCurrentDocument", System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance).SetValue(obj, value);
     }
-
-    private System.IServiceProvider serviceProvider;
 }
 
 [System.Runtime.InteropServices.Guid("65C44EF9-16F8-4F36-BD73-F10335EC452E")]
